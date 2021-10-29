b4b728f4a13473323a480ea5a7712a4e845e9b94
RickardVMiun <svenstaviken@hotmail.com>
2021-10-29 13:37:40 +0200

    Skapat möjlighet för att ogilla en film och få resultatet visat i realtid.

--- Cineasterna/wwwroot/js/site.js
+++ Cineasterna/wwwroot/js/site.js
@@ -1,9 +1,8 @@
 ﻿var likeresponse = {};
 
+// Logik för att hantera likes av en specifik film.
+
 document.querySelector('#upvoteButton').onclick = function () { like() };
-
-
-
 function like() {
     var imdbID = getMyValue();
     
@@ -21,8 +20,31 @@ function like() {
     div.innerHTML = "<strong>Tack</strong> för din röst";
 
     document.querySelector('#movie-information').append(div);
-   
 }
+
+// Logik för att hantera dislikes av en specifik film.
+
+document.querySelector('#downvoteButton').onclick = function () { dislike() };
+function dislike() {
+    var imdbID = getMyValue();
+
+    fetch("https://grupp9.dsvkurs.miun.se/api/" + imdbID + "/dislike")
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#cMDBvaluedown').innerHTML = data.numberOfDislikes
+        });
+
+    document.querySelector('#upvoteButton').disabled = true;
+    document.querySelector('#downvoteButton').disabled = true;
+
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "<strong>Tack</strong> för din röst";
+
+    document.querySelector('#movie-information').append(div);
+}
+
+
 
 
 function getMyValue() {
